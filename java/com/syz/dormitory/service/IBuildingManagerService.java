b6d86c0753fc9f18b30a241dd14a115d4beaf0f2
xiaoooobai <136244807+xiaoooobai@users.noreply.github.com>
2024-10-24 11:25:56 +0800

    Delete java/com/syz/dormitory directory

--- java/com/syz/dormitory/service/IBuildingManagerService.java
+++
@@ -1,9 +0,0 @@
-package com.syz.dormitory.service;
-
-import com.syz.dormitory.pojo.BuildingManager;
-
-public interface IBuildingManagerService {
-    boolean add(BuildingManager buildingManager);
-    boolean change(BuildingManager buildingManager);
-    boolean delete(BuildingManager buildingManager);
-}
--- java/com/syz/dormitory/service/IStuDorService.java
+++
@@ -1,9 +0,0 @@
-package com.syz.dormitory.service;
-
-import com.syz.dormitory.pojo.StuDor;
-
-public interface IStuDorService {
-    boolean add(StuDor stuDor);
-    boolean change(StuDor stuDor);
-    boolean delete(StuDor stuDor);
-}
